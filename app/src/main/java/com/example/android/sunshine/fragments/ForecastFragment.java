7c3fb9fc6504e12e6877eb73a5ea568505056e95
reginelae <reginelae>
2016-03-18 19:01:59 -0700

    third commit

--- app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
@@ -13,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.R;
 
@@ -96,6 +98,15 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_Forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         return rootView;
     }
